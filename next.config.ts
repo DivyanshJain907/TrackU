5ff0008d2623e422b97e5d1edc4272e5302890fa
divyansh907 <divyanshjaindpsschool@gmail.com>
2026-02-05 20:09:41 +0530

    refactor: remove serverRuntimeConfig and bodyParser size limit from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    bodyParser: {
-      sizeLimit: '5mb',
-    },
-  },
 };
 
 export default nextConfig;
